5f29e8211b5f36c58fb907c73ebbb906a3d8373c
Ni <Phanterasonpardus797@gmail.com>
2019-10-05 16:19:37 +0700

    update file

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // Latihan artmatika 
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const array_lib = require("./array");
+const object_lib = require("./object");
+const array_routes = require9("./routes");
+const array_sort = require("./lib/task1");
 
 // untuk memberikan parsing   
 app.use(express.urlencoded({extended: true}))
 
 app.get("/Hello", (req,res) => {
-    console.log("Hello Smart Man");
+    
 });
 
 app.listen(3300, () => {
