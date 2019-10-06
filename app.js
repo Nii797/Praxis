c4b9633491aad67154214137e0f0d1c2f0ae31c4
Ni <Phanterasonpardus797@gmail.com>
2019-10-06 10:38:58 +0700

    update file app.js

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 
 app.get("/tes-get", (req, res) => {
-
+    console.log("Hello Smart people");
 });
+
+app.post("/for", (req, res) => {
+    var i;
+    for(i=0; i < cars.length; i++) {
+        
+    }
+})
 
 app.listen(3300, () => {
     console.log('Example app listening on port 3300');
