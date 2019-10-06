8ff552678a4beec379f8f97ae365c06f36cc6249
Ni <Phanterasonpardus797@gmail.com>
2019-10-06 11:11:04 +0700

    update file app.js

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ app.get("/tes-get", (req, res) => {
     console.log("Hello Smart people");
 });
 
-app.post("/for", (req, res) => {
-    var i;
-    for(i=0; i < cars.length; i++) {
-        
+app.get("/for", (req, res) => {
+    var array = ['a', 'b', 'c'];
+    for (var i=0; 1<array.length; i++) {
+        console.log(array[i]);
     }
+
+    return res.send(array);
 })
 
 app.listen(3300, () => {
